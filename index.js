56c8151d2cc77689bd91daab802f3f1622c54989
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 17:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function  (){
 
 function searchRepositories () {
   $.get(`https://api.github.com/search/repositories?q=${document.getElementById('searchTerms').value}`, function(data) {
-    console.log(data)
+  
     for (const x of data.items) {
       console.log(x)
       $('#results').append(`<div>${x.name}, <a href="#" data-repository="${x.name}" onclick="showCommits(this)">Show Commits</a></div>`)
@@ -13,7 +13,6 @@ function searchRepositories () {
 }
 
 function showCommits(el) {
-  console.log(el)
   $.get(`https://api.github.com/repos/chvin/react-tetris/commits`, function(data){
 
   })
