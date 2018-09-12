5bf716b03def9677e1cae16b1a0a92e5a9439de5
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 16:52:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ $(document).ready(function  (){
 function searchRepositories () {
   $.get(`https://api.github.com/search/repositories?q=${document.getElementById('searchTerms').value}`, function(data) {
     console.log(data)
-
+    for (const x of data.items) {
+      $('#results').append(`<div>${x.name}</div>`)
+    }
   })
 }
