07c2494dacc7066c08dcb2d43c9ef051095075e9
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 17:19:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function searchRepositories () {
   $.get(`https://api.github.com/search/repositories?q=${document.getElementById('searchTerms').value}`, function(data) {
     console.log(data)
     for (const x of data.items) {
-      $('#results').append(`<div>${x.name}, <a href="#" data-repo="${x.name}" onclick="showCommits(this)">Show Commits</a></div>`)
+      $('#results').append(`<div>${x.name}, <a href="#" data-repository="${x.name}" onclick="showCommits(this)">Show Commits</a></div>`)
     }
   })
 }
